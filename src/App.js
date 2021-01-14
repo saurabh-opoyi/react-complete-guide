4dbf06712d9151c694e9ee15776ebb8048eff57b
saurabh-opoyi <sggoyal@gmail.com>
2021-01-14 22:38:11 +0530

    Passing method refrence B/wcomponents

--- src/App.js
+++ src/App.js
@@ -32,9 +32,17 @@ const app = props => {
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
        <button onClick={switchNameHandler}>Switch Name</button>
-       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Chess</Person>
-       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+       <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}/>
+       <Person click={switchNameHandler}
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}>
+            My Hobbies: Chess
+       </Person>
+       <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age}/>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi, I\'m a React App!!!'))
